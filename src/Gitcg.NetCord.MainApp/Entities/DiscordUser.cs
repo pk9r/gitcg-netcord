ab7982b373cbb39451f4394851032d4702c73951
La Minh Phuc <72692627+pk9r@users.noreply.github.com>
2025-08-07 16:03:29 +0700

    Add entity models and DbContext for Hoyolab integration

---
+++ src/Gitcg.NetCord.MainApp/Entities/DiscordUser.cs
@@ -0,0 +1,37 @@
+﻿namespace Gitcg.NetCord.MainApp.Entities;
+
+public class DiscordUser
+{
+    public ulong Id { get; set; }
+
+    public ICollection<HoyolabAccount> HoyolabAccounts { get; set; } = [];
+    public ActiveHoyolabAccount? ActiveHoyolabAccount { get; set; }
+
+    public Task AddHoyolabAccountAsync(
+        string hoyolabUserId, string token, string gameRoleId, string region
+    )
+    {
+        var hoyolabAccount = new HoyolabAccount
+        {
+            HoyolabUserId = hoyolabUserId, //
+            Token = token, //
+            GameRoleId = gameRoleId, //
+            Region = region //
+        };
+
+        HoyolabAccounts.Add(hoyolabAccount);
+
+        if (
+            ActiveHoyolabAccount == null ||
+            ActiveHoyolabAccount.HoyolabAccountId == Guid.Empty
+        )
+        {
+            ActiveHoyolabAccount = new ActiveHoyolabAccount
+            {
+                HoyolabAccount = hoyolabAccount //
+            };
+        }
+
+        return Task.CompletedTask;
+    }
+}
